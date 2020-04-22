0c407ac41553b7685ddc782088e9774ce4ec3514
rahat14 <rahatshovo6666@gmail.com>
2020-04-22 06:16:07 -0700

    Initial Commit

---
+++ walify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  walify
+//
+//  Created by rahat shovo  on 22/4/20.
+//  Copyright © 2020 rahat shovo . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
